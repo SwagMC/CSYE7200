6ab2d767cc4e905c5e6d568bf08b393017161689
Chao Ma <ma.cha@northeastern.edu>
2020-02-20 02:10:38 -0500

    assignment 4 Random State

--- assignment-random-state/src/main/scala/edu/neu/coe/csye7200/asstrs/RandomState.scala
+++ assignment-random-state/src/main/scala/edu/neu/coe/csye7200/asstrs/RandomState.scala
@@ -36,14 +36,14 @@ trait RandomState[T] {
     */
   // Hint: Think of the input and output, find the appropriate method that achieve this.
   // 10 points
-  def flatMap[U](f: T=>RandomState[U]): RandomState[U] = ??? // TO BE IMPLEMENTED
+  def flatMap[U](f: T=>RandomState[U]): RandomState[U] = f(get) // TO BE IMPLEMENTED done
 
   /**
     * @return a stream of T values
     */
   // Hint: This a recursively method and it concatenate current element with following elements.
   // 12 points
-  def toStream: Stream[T] = ??? // TO BE IMPLEMENTED
+  def toStream: Stream[T] = Stream.cons(this.get, this.next.toStream) // TO BE IMPLEMENTED done
 }
 
 /**
@@ -55,13 +55,14 @@ trait RandomState[T] {
 case class JavaRandomState[T](n: Long, g: Long=>T) extends RandomState[T] {
   // Hint: Remember to use the "seed" to generate next RandomState.
   // 7 points
-  def next: RandomState[T] = ??? // TO BE IMPLEMENTED
+  val r = new Random(n)
+  def next: RandomState[T] = JavaRandomState(r.nextLong(), g) // TO BE IMPLEMENTED done
   // Hint: Think of the input and output.
   // 5 points
-  def get: T = ??? // TO BE IMPLEMENTED
+  def get: T = g(n) // TO BE IMPLEMENTED done
   // Hint: This one need function composition.
   // 13 points
-  def map[U](f: (T) => U): RandomState[U] = ??? // TO BE IMPLEMENTED
+  def map[U](f: (T) => U): RandomState[U] = JavaRandomState[U](n, n => f(g(n))) // TO BE IMPLEMENTED done
 }
 
 case class DoubleRandomState(n: Long) extends RandomState[Double] {
@@ -83,7 +84,7 @@ object RandomState {
   def apply(): RandomState[Long] = apply(System.currentTimeMillis)
   // Hint: This is a easy one, remember that it not only convert a Long to a Double but also scale down the number to -1 ~ 1.
   // 4 points
-  val longToDouble: Long=>Double = ??? // TO BE IMPLEMENTED
+  val longToDouble: Long=>Double = _.toDouble / Long.MaxValue // TO BE IMPLEMENTED done
   val doubleToUniformDouble: Double=>UniformDouble = {x => UniformDouble((x+1)/2)}
 }
 
